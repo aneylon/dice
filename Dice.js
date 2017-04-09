7de65db0f75c5d33e23d5cb6c7b67970abd3d74f
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-04-09 13:47:37 -0600

    total overhaul

--- Dice.js
+++ Dice.js
@@ -1,32 +1,79 @@
-document.getElementById('dice-form').addEventListener('submit', function(event){
-  event.preventDefault()
+document.getElementById('diceAddForm').addEventListener('submit', function(e){
+  e.preventDefault()
 })
 
-function rollDice(){
-  var number = Number(document.getElementById('numberOfDice').value)
-  var max = Number(document.getElementById('maxNumber').value)
+let diceRoller = (function(){
+  let dice = []
+  let diceCollection = document.getElementById('diceCollection')
+  let numberOfDiceEl = document.getElementById('numberOfDice')
+  let maxValueOfDiceEl = document.getElementById('maxValueOfDice')
+  let modifierEl = document.getElementById('modifier')
+  let messageEl = document.getElementById('message')
 
-  document.getElementById('result').innerHTML = getResult(number, max)
-}
+  numberOfDiceEl.focus()
 
-function getResult(num, max){
-  var mod = Number(document.getElementById('modNumber').value)
-  console.log(mod)
-  if(isNaN(num) || num < 1){
-    return 'Please enter a number of dice, 1 or greater.'
+  let add = function() {
+    // error checking -numbers
+    let numberOfDice = numberOfDiceEl.value
+    numberOfDiceEl.value = ''
+
+    let maxValueOfDice = maxValueOfDiceEl.value
+    maxValueOfDiceEl.value = ''
+
+    if(maxValueOfDice < 1 || numberOfDice < 1){
+      messageEl.innerHTML = 'please enter only positive numbers'
+    } else {
+      messageEl.innerHTML = ''
+      dice.push({ number: numberOfDice, maxValue: maxValueOfDice })
+    }
+
+    numberOfDiceEl.focus()
+    RenderDice()
   }
 
-  if(isNaN(max) || max < 2){
-    return 'Please enter a maximum number, 2 or greater.'
+  let roll = function() {
+    result.innerHTML = ''
+    let total = 0
+
+    dice.forEach(die => {
+      for(var i = 0; i < die.number; i++){
+        total += getRand(die.maxValue)
+      }
+    })
+
+    total += Number(modifierEl.value)
+    setTimeout(function(){
+      result.innerHTML = total
+    },500)
   }
 
-  var total = 0
-  for(var i = 1; i <= num; i++){
-    total += getRand(max)
+  let clear = function() {
+    dice = []
+    diceCollection.innerHTML = ''
+    modifierEl.value = ''
+    numberOfDiceEl.value = ''
+    maxValueOfDiceEl.value = ''
+    result.innerHTML = ''
+    numberOfDiceEl.focus()
   }
-  return total + mod
-}
 
-function getRand(maxNum){
-  return Math.floor((Math.random() * maxNum) + 1)
-}
+  function RenderDice() {
+    let output = ''
+
+    dice.forEach(die => {
+      output += `<p><span>${die.number}</span><span>d</span><span>${die.maxValue}</span></p>`
+    })
+
+    diceCollection.innerHTML = output
+  }
+
+  function getRand(maxNum){
+    return Math.floor((Math.random() * maxNum) + 1)
+  }
+
+  return {
+    add,
+    roll,
+    clear
+  }
+})()
--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Simple dice rolling app.
 [Live App](http://www.arlen-neylon.com/dice)
 
 ## todos
-- [ ] dice of different denominations.
 - [ ] device specific styling.
 - [ ] extra credit
   - [ ] custom face dice.
